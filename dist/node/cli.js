9333dee374e845950960dae59f50c92d2dd086c5
HHX <903040380@qq.com>
2023-04-20 14:40:49 +0800

    feat: 完成cli与dev server搭建

---
+++ dist/node/cli.js
@@ -0,0 +1,22 @@
+"use strict";
+// 脚手架
+Object.defineProperty(exports, "__esModule", { value: true });
+const cac_1 = require("cac");
+const dev_1 = require("./dev");
+// 配置命令
+const cli = (0, cac_1.default)('hhxpress');
+cli
+    .command('[root]', 'Start Dev Server')
+    .alias('dev')
+    .action(async (root) => {
+    // 创建vite静态服务
+    const server = await (0, dev_1.createViteServer)(root);
+    // 开启监听
+    await server.listen();
+    // 打印服务url
+    server.printUrls();
+});
+cli.command('build [root]', 'Start Build').action((root) => {
+    console.log(root);
+});
+cli.parse();
---
+++ src/node/cli.ts
@@ -0,0 +1,28 @@
+// 脚手架
+
+import cac from 'cac'
+import { createViteServer } from './dev'
+
+// 配置命令
+
+const cli = cac('hhxpress')
+
+cli
+  .command('[root]', 'Start Dev Server')
+  .alias('dev')
+  .action(async (root) => {
+    // 创建vite静态服务
+    const server = await createViteServer(root)
+
+    // 开启监听
+    await server.listen()
+
+    // 打印服务url
+    server.printUrls()
+  })
+
+cli.command('build [root]', 'Start Build').action((root) => {
+  console.log(root)
+})
+
+cli.parse()
