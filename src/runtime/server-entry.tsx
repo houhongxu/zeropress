dbba2e8e103ddb3bc793ca3ef02f7f247e94dfc7
HHX <903040380@qq.com>
2023-05-04 16:47:46 +0800

    fix: 修复服务端渲染路由未提供location的问题

--- src/runtime/server-entry.tsx
+++ src/runtime/server-entry.tsx
@@ -5,11 +5,12 @@ import { App } from './App'
 import { StaticRouter } from 'react-router-dom/server'
 import routes from 'virtual:routes'
 
-export function renderInServer() {
+export function renderInServer(routePath: string) {
   return renderToString(
-    // TODO location应该是根据客户端url
     // https://reactrouter.com/en/main/router-components/static-router
-    <StaticRouter location={'/'}>
+    // BrowserRouter 使用的是 History API 记录位置，而 History API 是属于浏览器的 API ，在 SSR 的环境下，服务端不能使用浏览器 API
+    // 通过初始传入的 location 地址找到相应组件
+    <StaticRouter location={routePath}>
       <App></App>
     </StaticRouter>,
   )
