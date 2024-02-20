fc186a63158b19888602f1a7ca0068477507164c
HHX <903040380@qq.com>
2024-02-20 11:09:59 +0800

    feat: 支持mdx热更新

--- apps/examples/docs/count.tsx
+++ apps/examples/docs/count.tsx
@@ -1,3 +1,13 @@
-export default function () {
-  return <div>count</div>
+import { useState } from 'react'
+
+export const a = { a: 2 }
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount((count) => count + 1)}>点击加1</button>
+    </div>
+  )
 }
--- packages/easypress/lib/cli.cjs
+++ packages/easypress/lib/cli.cjs
@@ -220,15 +220,17 @@ function createPlugins({
   restartRuntimeDevServer
 }) {
   return [
-    (0, import_plugin_react.default)(),
+    { enforce: "pre", ...createRollupPluginMdx() },
+    // 结合mdx与vite-plugin-react https://github.com/vitejs/vite-plugin-react/issues/38
+    (0, import_plugin_react.default)({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+    // 根据热更新规则，仅导出组件时才能保证热更新正确，因为组件副作用是确定的，所以mdx导出frontmatter等对象会提示但是仍然尝试生效，这规则是react refresh特性，所以webpack热更新插件也是这样 https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md#consistent-components-exports https://github.com/pmmmwh/react-refresh-webpack-plugin/issues/249#issuecomment-729277683
     vitePluginServeHtml({
       templatePath: HTML_PATH,
-      // /@fs/是针对root之外的，当作为npm包时在nodemodules中属于root内，不需要使用 https://cn.vitejs.dev/config/server-options.html#server-fs-allow
       entry: CLIENT_ENTRY_PATH
+      // /@fs/是针对root之外的，当作为npm包时在nodemodules中属于root内，不需要使用 https://cn.vitejs.dev/config/server-options.html#server-fs-allow
     }),
     vitePluginVirtualConfig({ siteConfig, restartRuntimeDevServer }),
-    vitePluginVirtualRoutes({ root }),
-    createRollupPluginMdx()
+    vitePluginVirtualRoutes({ root })
   ];
 }
 
