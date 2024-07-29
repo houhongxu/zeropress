5252f36de93812cb3d23c12b219638beabd92b23
houhongxu <hongxu.hou@goodwe.com>
2024-07-29 13:50:23 +0800

    fix: 修复一些问题

--- packages/zeropress/src/node/build.ts
+++ packages/zeropress/src/node/build.ts
@@ -47,6 +47,7 @@ function viteBuild({
 }) {
   return build({
     mode: 'production',
+    root: ROOT_PATH, // 获取tsconfig.json等配置文件
     plugins: createPlugins({ siteConfig }),
     build: {
       ssr: isServer,
